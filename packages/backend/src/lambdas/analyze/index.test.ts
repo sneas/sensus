a4625af551a201cb1431369bfe5c6e7ce1767d46
sergey-tom <sergii.tomusiak@icloud.com>
2023-11-05 15:47:32 +0100

    Implement initial analyze endpoint

--- packages/backend/src/lambdas/analyze/index.test.ts
+++ packages/backend/src/lambdas/analyze/index.test.ts
@@ -2,10 +2,14 @@ import { analyze } from './index';
 import {APIGatewayProxyEvent} from "aws-lambda";
 
 // @ts-ignore
-let mockEvent: APIGatewayProxyEvent = {};
+let mockEvent: APIGatewayProxyEvent = {
+  body: '{"comment": "This is terrible suggestion!"}',
+};
+
 describe('analyze', () => {
   it('is working', async () => {
     const result = await analyze(mockEvent);
+    console.log(JSON.stringify(result));
     expect(result.statusCode).toEqual(200);
-  })
+  }, 60_000)
 });
