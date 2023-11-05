71e09af52a1ec0f27cea949e3f14ba11805e4c59
Dima Snisarenko <snisarenkodima@gmail.com>
2023-11-05 15:48:35 +0100

    chore: extension content script

---
+++ packages/extension-content-script/src/index.ts
@@ -0,0 +1,2 @@
+export const registerContentScript = async () => {
+};
---
+++ packages/extension-content-script/webpack.config.js
@@ -0,0 +1,35 @@
+const { basename } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { WatchTimerPlugin } = require('@sensus/webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './public/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /src\/.+\.html/,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: true,
+    }),
+    new WatchTimerPlugin(basename(__dirname)),
+  ],
+  devServer: {
+    port: 8020,
+  },
+};
